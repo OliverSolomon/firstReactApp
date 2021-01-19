62e2a01cf1dcc9beec9fc7f99c7649ab0f4884b6
oliv3r <gamin3@kali.olly>
2021-01-19 16:54:58 +0300

    custom tweets

--- src/App.js
+++ src/App.js
@@ -8,16 +8,17 @@ function App() {
         console.log('hello');
 
     }
+    // var names = ['oliver', 'jane', 'Saina', 'Brian', 'Keith', 'Local Man'];
     return (
         <div className='mainContainer'>
             <Hello/>
             <div className='tweetsContainer'>
-                <Tweet/>
-                <Tweet/>
-                <Tweet/>
-                <Tweet/>
-                <Tweet/>
-                <Tweet/>
+                <Tweet name='Oliver' message = 'I am the greatest but still myopic Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!' />
+                <Tweet name='Jane'message = 'To be or not to be is one of the hardest existential questions Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!'/>
+                <Tweet name= 'saina' message = 'She loves me, She loves me not.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!'/>
+                <Tweet name = 'Brian' message = "Doesn't it frigten you to find out that you dont know yourself too well ? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!"/>
+                <Tweet name = 'Keith' message = "Baby, you're the best i ever had.  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!"/>
+                <Tweet name = 'Local Man' message = 'Local Man is scared stiff of politics. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!'/>
             </div>
             {/* <h2>Hello React</h2>
             <button onClick={sayhello}>Hello React</button> */}
--- src/students.js
+++ src/students.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
-function Tweet() {
+// function Tweet(props) {
     
-    var name = "Oliver";
+//     var name = "Oliver";
+//     var date = "23/12/2001";
+//     var content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!";
+//     return ( 
+//         <div className="Tweets">
+//             <div className="tweet">
+//                 <div className="headTweet">
+//                     <div className="Author"> {props.name} </div>
+//                     <div className="Date">{date}</div>
+//                 </div>
+//                 <div className="content">{content}</div>
+//             </div>
+//         </div>
+//     );
+
+// }
+
+function Tweet({name, message}) {
+    
+    // var name = "Oliver";
     var date = "23/12/2001";
-    var content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!";
+    // var content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero debitis harum voluptates!";
+    var content = message;
     return ( 
         <div className="Tweets">
             <div className="tweet">
